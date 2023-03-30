57990e363a5305fbbea0387fb33540b9493c6e9c
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-03-30 23:37:04 +0600

    get card data

---
+++ src/componnents/Blog/Blog.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Blog.css'
+
+const Blog = (props) => {
+     console.log(props.blog);
+    const {author_name, blog_title, cover_img, hash_tag, pro_images, publish_date, read_time} = props.blog;
+    return (
+        <div>
+            <div>
+                <img className='cover' src={cover_img} alt="" />
+                <div className='author_container'>
+                    <div className='blog_data'>
+                        <div className='pro_img'>
+                            <img  src={pro_images} alt="" />
+                        </div>
+                        <div className='author_info'>
+                            <h3>{author_name}</h3>
+                            <p><small>{publish_date}</small></p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>{read_time} min read</p>
+                    </div>
+                </div>
+                <h2>{blog_title}</h2>
+                <p>{hash_tag}</p>
+                <button>Mark as Read</button>
+            </div>
+            <hr />
+        </div>
+    );
+};
+
+export default Blog;
--- src/componnents/Card/Card.jsx
+++ src/componnents/Card/Card.jsx
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import Blog from '../Blog/Blog';
 import './Card.css'
 const Card = () => {
 
-    const [blog, setBlog] = useState([])
+    const [blogs, setBlog] = useState([])
 
     useEffect(()=>{
         fetch('blogdata.json')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setBlog(data))
     },[])
 
     return (
         <div>
             <div className='shop-container'>
             <div className="product-container">
+                {/* <h1>{blogs.length}</h1> */}
+
                 {
-                
+                    blogs.map(blog => <Blog
+                    key = {blog.id}
+                    blog = {blog}
+                    ></Blog>)
                 }
                 
             </div>
